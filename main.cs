80a6577602058bf0234674b34444d45a120c22a9
liz22 <chapiz_19_08@hotmail.com>
2013-10-17 22:53:00 -0500

    creamos ruta del archivo

---
+++ main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Practica4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Programa ob = new Programa();
+			string ruta;
+
+			Console.WriteLine("Ingrese la ruta del archivo: ");
+			ruta=Console.ReadLine();
+
+			ob.leerArchivo(Path.GetFullPath(ruta));
+		}
+	}
+}

487f6d4fa674b66f5255d64532a1c56222e22e6e
liz22 <chapiz_19_08@hotmail.com>
2013-10-17 22:52:28 -0500

    datos a leer

---
+++ datos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practica4
+{
+	public class datos
+	{
+		public string id;
+		public string nombre;
+		public string domicilio;
+		public string telefono;
+
+		public datos ()
+		{
+		}
+
+		public void imprimir(){
+			Console.Write("Id: ");
+			Console.WriteLine(this.id);
+			Console.Write("Nombre: ");
+			Console.WriteLine(this.nombre);
+			Console.Write("Domicilio: ");
+			Console.WriteLine(this.domicilio);
+			Console.Write("Telefono: ");
+			Console.WriteLine(this.telefono);
+		}
+	}
+}
+
